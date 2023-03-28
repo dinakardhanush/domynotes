2c67e687572ea89a7bb45410450e896657299ee2
dinakardhanush <dinakardhanush01@gmail.com>
2023-03-28 13:23:58 +0530

    2nd commit

--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from django.db.models import Q
-
 from . import models, serializers
+
 
 
 @api_view(['POST'])
